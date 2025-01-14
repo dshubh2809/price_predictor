bb5bae9260fcbcda8de395ea41c649ebeaed12b9
Shubh Dwivedi <117860550+dshubh2809@users.noreply.github.com>
2025-01-15 03:26:31 +0530

    lstm_model.py

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 
 def build_lstm_model(input_shape):
-    """
-    Build an LSTM model for price prediction.
-    """
+    
     model = tf.keras.Sequential([
         tf.keras.layers.LSTM(50, return_sequences=True, input_shape=input_shape),
         tf.keras.layers.LSTM(50),
